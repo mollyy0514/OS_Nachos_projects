f125bea425bbc5865ce619e11111fb3c80a17b88
mollyy0514 <91655983+mollyy0514@users.noreply.github.com>
2023-12-24 15:57:14 +0800

    return the case RR to List

--- nachos-4.0/code/threads/scheduler.cc
+++ nachos-4.0/code/threads/scheduler.cc
@@ -64,9 +64,9 @@ Scheduler::Scheduler(SchedulerType type)
 	schedulerType = type;
 	switch(schedulerType) {
         case RR:
-            // readyList = new List<Thread *>();
+            readyList = new List<Thread *>();
             /* Morris 的寫法 */
-            readyList = new SortedList<Thread *>(FIFOCompare);
+            // readyList = new SortedList<Thread *>(FIFOCompare);
             /* Morris 的寫法 */
             break;
         case SJF:
